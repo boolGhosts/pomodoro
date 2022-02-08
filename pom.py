65466ae37ad3d3a420ceed515d598d2841026577
Ryan Condon <rtcondon@protonmail.com>
2022-02-07 17:06:33 -0800

    sound

--- pom.py
+++ pom.py
@@ -1,12 +1,12 @@
 #! Python3
 #pom.py pomodoro timer
 
-import time datetime
+import time, datetime, subprocess
 
 # Display user instructions
 activeTask = input('Enter name of task> ')
 pomDur = 25
-pomDur = int(input('Enter pom minutes [20]> ') or "25")
+pomDur = int(input('Enter task time [25] min > ') or "25")
 print(f'{activeTask} is the task.  Press enter to start pomodoro timer')
 input()
 print('Started')
@@ -23,19 +23,25 @@ def pomodoro(n):
       print(f'{elipse}...')
       time.sleep(.1)
   print('Times up')
+  subprocess.Popen(['start', 'alarm.wav'], shell=True)
   pomNum = pomNum + 1
   return(pomNum)
 
-for i in range(4):
-  count = pomodoro(i)
-  print(f'You\'ve done {count} poms')
-  print(f'{activeTask} is the task.  Press enter to start pomodoro timer')
-  input()
-  print('Started')
+def main():
+    for i in range(4):
+        count = pomodoro(i)
+        print(f'You\'ve done {count} poms')
+        if i < 3:
+            print(f'{activeTask} is the task.  Press enter to start pomodoro timer')
+            input()
+            print('Started')
+    return()
 
-print('Break until ')
-
-
-
- 
-
+while True:
+    main()
+    lnbreakmin = int(input('Long break time! How long? [15]min > ') or "15")
+    lnbreakcalc = datetime.datetime.fromtimestamp(time.time() + lnbreakmin*60)
+    print(f"Break until {lnbreakcalc.strftime('%H:%M')}.")
+    time.sleep(lnbreakmin*60)
+    print(f'Break over! {activeTask} is the task.  Press enter to start pomodoro timer')
+    input()
